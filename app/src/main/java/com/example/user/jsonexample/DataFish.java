14af43d1c733805efdf2de7218f490d8ed7e18ea
niropam <niropam_engr@outlook.com>
2018-03-07 21:22:39 +0600

    Initial commit for Simple JSon Project

---
+++ app/src/main/java/com/example/user/jsonexample/DataFish.java
@@ -0,0 +1,13 @@
+package com.example.user.jsonexample;
+
+/**
+ * Created by User on 3/6/2018.
+ */
+
+class DataFish {
+    public String fishImage;
+    public String fishName;
+    public String catName;
+    public String sizeName;
+    public int price;
+}
